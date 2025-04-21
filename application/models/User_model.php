3e25dd35c40d963d410498fd8cf1c04e1679d5e2
Stelios Panselinas <112862638+Stelios-Panselinas@users.noreply.github.com>
2025-04-21 10:30:09 +0300

    changes for operations
    changes to show only the operations that correspond to the subgroup of the member

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -62,10 +62,40 @@
             return $query->result_array();
         }
 
-        public function get_operations($id){
+        public function get_operations($id) {
+            if($this->session->userdata('subgroup_id') == 7) {
+                $this->db->join('operation', 'participate_in_operation.operation_id = operation.operation_id', 'inner');
+                $query = $this->db->get_where('participate_in_operation', array('participate_in_operation.user_id' => $id));
+            }else{
+                // Subquery 1: Get user IDs belonging to the subgroup
+            $this->db->select('belong_to_subgroup.user_id');
+            $this->db->from('belong_to_subgroup');
+            $this->db->join('user', 'belong_to_subgroup.user_id = user.user_id', 'inner');
+            $this->db->where('belong_to_subgroup.subgroup_id', $this->session->userdata('subgroup_id'));
+            $subquery1 = $this->db->get_compiled_select();
+        
+            // Subquery 2: Get operation IDs for users in the subgroup
+            $this->db->select('participate_in_operation.operation_id');
+            $this->db->from('participate_in_operation');
             $this->db->join('operation', 'participate_in_operation.operation_id = operation.operation_id', 'inner');
-            $query = $this->db->get_where('participate_in_operation', array('participate_in_operation.user_id' => $id));
-            return $query->result_array();
+            $this->db->where("participate_in_operation.user_id IN ($subquery1)", null, false);
+            $subquery2 = $this->db->get_compiled_select();
+        
+            // Subquery 3: Get operation IDs linked to the subgroup
+            $this->db->select('operation_to_subgroup.operation_id');
+            $this->db->from('operation_to_subgroup');
+            $this->db->where("operation_to_subgroup.operation_id IN ($subquery2)", null, false);
+            $this->db->where('operation_to_subgroup.subgroup_id', $this->session->userdata('subgroup_id'));
+            $subquery3 = $this->db->get_compiled_select();
+        
+            // Main query: Get operations
+            $this->db->select('*');
+            $this->db->from('operation');
+            $this->db->where("operation.operation_id IN ($subquery3)", null, false);
+            $query = $this->db->get();
+        
+            return $query->result_array();;
+            }
         }
 
         public function get_trainings($id){
