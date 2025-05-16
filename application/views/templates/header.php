a4c2fa8a8318d3681949cebc0654e7b68c271032
Stelios Panselinas <112862638+Stelios-Panselinas@users.noreply.github.com>
2025-05-16 15:37:29 +0300

    add slash on url path

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,7 +2,7 @@
 
 <head>
     <title>ΕΟΔ Αχαΐας</title>
-	<link rel="icon" type="image/x-icon" href="assets/img/hrt_logo.png">
+	<link rel="icon" type="image/x-icon" href="/assets/img/hrt_logo.png">
     <!-- Latest compiled and minified CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Latest compiled JavaScript -->
