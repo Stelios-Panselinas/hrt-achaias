d183a918609301779013b11d20b38b0f15322853
Stelios Panselinas <112862638+Stelios-Panselinas@users.noreply.github.com>
2025-05-16 15:28:29 +0300

    change page title

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,7 +1,7 @@
 <html>
 
 <head>
-    <title>Hrt-Achaias</title>
+    <title>ΕΟΔ Αχαΐας</title>
 	<link rel="icon" type="image/x-icon" href="assets/img/hrt_logo.png">
     <!-- Latest compiled and minified CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
