e3bc383b2dc9dadeacb6780be66a98805d26e8a6
Stelios Panselinas <112862638+Stelios-Panselinas@users.noreply.github.com>
2025-05-16 15:26:15 +0300

    add favicon

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,6 +2,7 @@
 
 <head>
     <title>Hrt-Achaias</title>
+	<link rel="icon" type="image/x-icon" href="assets/img/hrt_logo.png">
     <!-- Latest compiled and minified CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Latest compiled JavaScript -->
